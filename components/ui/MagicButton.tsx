0578044e2ecca0d74b4a6642622eeb8269f22ec9
Rudzani Mudau <132647693+kxngHADES@users.noreply.github.com>
2025-01-25 20:17:16 +0200

    Pre

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -7,10 +7,10 @@ const MagicButton = ({
 
 ) => {
   return (
-    <button className="inline-flex items-center gap-2 px-12 py-8 rounded-full bg-[#1ED760] font-bold text-white tracking-widest uppercase transform hover:scale-110 hover:bg-[#21e065] transition-colors duration-200">
-        {position === "left" && icon}
+    <button className={`inline-flex items-center justify-center gap-2 px-8 py-4 rounded-full bg-green-500 hover:bg-green-800 font-bold text-white tracking-wide transition-colors duration-200 ${otherClasses}`}>
+        {/*{position === "left" && icon}*/}
         {title}
-        {position === "right" && icon}
+        {/*{position === "right" && icon}*/}
     </button>
   )
 }
